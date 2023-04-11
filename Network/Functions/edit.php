d01a8ba4ac4343f132ace3debdbf1a4e01b72b68
JP1005YT <godlolpro32@gmail.com>
2023-04-10 21:14:19 -0300

    "Really Final?"

--- Network/Functions/edit.php
+++ Network/Functions/edit.php
@@ -10,7 +10,11 @@
             $sql = "UPDATE $dados[table] set nome = '$dados[nome]',email = '$dados[email]',telefone = '$dados[telefone]' where id = $dados[id]";
         }
     }else{
-        $sql = "UPDATE $dados[table] set nome = '$dados[nome]',descricao = '$dados[descricao]',preco = '$dados[preco]',quantidade = '$dados[quantidade]',vendedor_id = '$dados[vendedor_id]' where id = $dados[id]";
+        if($dados['cod'] != 1){
+            $sql = "UPDATE $dados[table] set pro_nome = '$dados[nome]',descricao = '$dados[descricao]',preco = '$dados[preco]',quantidade = '$dados[quantidade]',vendedor_id = '$dados[vendedor_id]' where id = $dados[id]";
+        }else{
+            $sql = "UPDATE $dados[table] set quantidade = '$dados[quantidade]' where id = $dados[id]";
+        }
     }
     $comando = mysqli_query($mysqli,$sql);
     echo json_encode($comando);
